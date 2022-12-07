451bcebf4098afcf377d050a4763860293cc2dfd
Gabriel Salazar <32026770+cubicles@users.noreply.github.com>
2022-12-06 20:43:00 -0500

    pesky automation

---
+++ day4.py
@@ -0,0 +1,24 @@
+# Advent of Code 2022
+#
+# created by Gabriel Salazar
+
+def part1(data_path: str):
+    with open(data_path) as file:
+        for line in file:
+            print(line)
+
+def part2(data_path: str):
+    with open(data_path) as file:
+        for line in file:
+            print(line)
+
+if __name__ == '__main__':
+    # input
+    data_path = 'input/day2.txt'
+
+    # part 1 + 2
+    print(part1(data_path))
+    print(part2(data_path))
+
+    print('Advent of Code 2022!')
+
